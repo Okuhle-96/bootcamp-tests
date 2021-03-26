5479edc2d728e8dde2664b6f4db9242c3afcb1c6
Okuhle Mhambi <bukhosimhambi@gmail.com>
2021-03-26 18:11:49 +0200

    please go live

--- test/regCheck.test.js
+++ test/regCheck.test.js
@@ -2,13 +2,13 @@ describe("regCheck function", function(){
     it("Should return true if regNo ends with GP", function(){
         assert.equal(regCheck('DV 23 NB GP', 'GP'), true)
     })
-    it("Should return FALSE if regNo ends with MP", function(){
+    it("Should return FALSE if regNo does not end with GP", function(){
         assert.equal(regCheck('DV 23 LP GP', 'MP'), false)
     })
-    it("Should return FALSE if regNo ends with GP", function(){
+    it("Should return FALSE if regNo end with GP", function(){
         assert.equal(regCheck('CY189-012', 'CY', 'MP'), false)
     })
-    it("Should return FALSE if regNo ends with ND", function(){
+    it("Should return FALSE if regNo does not end with GP", function(){
         assert.equal(regCheck('CY189-012', 'ND'), false)
     })
 
